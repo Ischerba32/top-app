fd473e262d98fa1af55869b5b4f17b12b4d0f6e8
Ischerba32 <i.scherba32@gmail.com>
2022-01-16 15:21:29 +0300

    added P, Rating, Tag components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
-import { Button, Htag } from "../components/";
+import { useState, useEffect } from "react";
+import { Button, Htag, P, Rating, Tag } from "../components/";
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
 
   return (
     <>
       <Htag tag='h1'>Hello</Htag>
       <Button appearance='primary'>Button</Button>
       <Button appearance='ghost' arrow='right'>Button</Button>
+      <P size="l">Large</P>
+      <P size="m">Middle</P>
+      <P size="s">small</P>
+      <Tag size='s'>smal</Tag>
+      <Tag size='m' color='red'>mid</Tag>
+      <Tag size='s' color='green'>green</Tag>
+      <Tag color='primary'>qwe</Tag>
+      <Rating rating={rating} isEditable setRating={setRating}/>
     </>
   );
 }

d9b9a3cd98deb7e3e41fb621a3406b5e6f625e6c
Ischerba32 <i.scherba32@gmail.com>
2022-01-17 14:57:25 +0300

    added Layout, SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { GetStaticProps } from "next";
 import { useState, useEffect } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components/";
+import { withLayout } from "../layout/Layout";
+import axios from 'axios';
+import { MenuItem } from "../interfaces/menu.interface";
 
-export default function Home(): JSX.Element {
+
+function Home({menu}: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
-
 
   return (
     <>
@@ -18,6 +22,33 @@ export default function Home(): JSX.Element {
       <Tag size='s' color='green'>green</Tag>
       <Tag color='primary'>qwe</Tag>
       <Rating rating={rating} isEditable setRating={setRating}/>
+      <ul>
+        {menu.map(m => {
+          return (
+            <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+          );
+        })}
+      </ul>
     </>
   );
+}
+
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>((process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find'), {
+    firstCategory
+  });
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
 }

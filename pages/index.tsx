2d832c46beaf3e53751308296563e9837b40a776
Ischerba32 <i.scherba32@gmail.com>
2022-02-01 16:53:43 +0300

    SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import { GetStaticProps, GetServerSideProps } from "next";
+import { GetServerSideProps } from "next";
 import { Htag } from "../components/";
 import { withLayout } from "../layout/Layout";
 import axios from 'axios';
 import { MenuItem } from "../interfaces/menu.interface";
 import { API } from "../helpers/api";
 
-function Home({menu}: HomeProps): JSX.Element {
+function Home({menu, temp}: HomeProps): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Выберите курс в необходимой категории</Htag>
@@ -14,34 +14,30 @@ function Home({menu}: HomeProps): JSX.Element {
           <li key={m._id.secondCategory}>{m._id.secondCategory} v </li>;
         })}
       </ul>
+      <div dangerouslySetInnerHTML={{__html: temp }}></div>
     </>
   );
 }
 
 export default withLayout(Home);
 
-// export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-// 	const firstCategory = 0;
-// 	const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
-//     firstCategory
-//   });
-// 	return {
-// 		props: {
-// 			menu,
-// 			firstCategory
-// 		}
-// 	};
-// };
 
 export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
   const firstCategory = 0;
 	const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
     firstCategory
   });
+  const temp =
+      `
+        <div>
+          <h2>Component from server</h2>
+        </div>
+      `;
 	return {
 		props: {
 			menu,
-			firstCategory
+			firstCategory,
+      temp,
 		}
 	};
 };
@@ -49,4 +45,5 @@ export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
 interface HomeProps extends Record<string, unknown> {
   menu: MenuItem[];
   firstCategory: number;
+  temp: string;
 }
